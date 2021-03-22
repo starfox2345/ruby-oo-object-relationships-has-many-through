8d737e45ffaf703b6caf364d7952ed81e4666ec6
yatsun <yatsunlam25@gmail.com>
2021-03-21 20:23:40 -0400

    commit tree 5c3ef1e963a4e1878898ae5f82e3e36d4214e946

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,38 @@
 class Waiter
 
+    attr_accessor :name, :exp
+    
+
+    @@all = []
+
+
+    def initialize(name, exp)
+        @name = name
+        @exp = exp
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meals| meals.waiter == self}
+    end
+
+    def best_tipper
+        tip = 0 
+        Meal.all.each do |meals|
+            if meals.tip > tip
+                tip = meals.tip
+            end
+        end
+        Meal.all.find {|meals| meals.tip == tip}.customer
+
+
+    end
 end
